7e7fe0f053bf1ad8573373c93c87b9a880443210
colobrodo <turbosmi3000@gmail.com>
2025-12-21 08:46:48 +0100

    Added fixed size estimator a la Zuckerli

---
+++ src/graphs/estimator/fixed_estimator.rs
@@ -0,0 +1,57 @@
+use std::convert::Infallible;
+
+use webgraph::prelude::*;
+
+pub struct FixedEstimator;
+
+impl Encode for FixedEstimator {
+    type Error = Infallible;
+
+    fn start_node(&mut self, _node: usize) -> Result<usize, Self::Error> {
+        Ok(0)
+    }
+
+    fn write_outdegree(&mut self, _value: u64) -> Result<usize, Self::Error> {
+        Ok(1)
+    }
+
+    fn write_reference_offset(&mut self, _value: u64) -> Result<usize, Self::Error> {
+        Ok(1)
+    }
+
+    fn write_block_count(&mut self, _value: u64) -> Result<usize, Self::Error> {
+        Ok(1)
+    }
+
+    fn write_block(&mut self, _value: u64) -> Result<usize, Self::Error> {
+        Ok(1)
+    }
+
+    fn write_interval_count(&mut self, _value: u64) -> Result<usize, Self::Error> {
+        Ok(1)
+    }
+
+    fn write_interval_start(&mut self, _value: u64) -> Result<usize, Self::Error> {
+        Ok(1)
+    }
+
+    fn write_interval_len(&mut self, _value: u64) -> Result<usize, Self::Error> {
+        Ok(1)
+    }
+
+    fn write_first_residual(&mut self, _value: u64) -> Result<usize, Self::Error> {
+        Ok(1)
+    }
+
+    fn write_residual(&mut self, _value: u64) -> Result<usize, Self::Error> {
+        Ok(1)
+    }
+
+    fn flush(&mut self) -> Result<usize, Self::Error> {
+        Ok(0)
+    }
+
+    fn end_node(&mut self, _node: usize) -> Result<usize, Self::Error> {
+        Ok(0)
+    }
+}
--- src/graphs/estimator/mod.rs
+++ src/graphs/estimator/mod.rs
@@ -1,5 +1,7 @@
+pub mod fixed_estimator;
 pub mod huffman_estimator;
 pub mod log2_estimator;
 
+pub use fixed_estimator::*;
 pub use huffman_estimator::*;
 pub use log2_estimator::*;
